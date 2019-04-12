232cf4cab08271b4da33d87dd999feac6e38be2d
Rickard <rickard.andersson98@gmail.com>
2019-04-12 14:33:54 +0200

    initial changes

---
+++ src/main/java/com/example/auction/MyWebMvcConfigurer.java
@@ -0,0 +1,16 @@
+package com.example.auction;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/static/")
+                .setCacheControl(CacheControl.noCache());
+    }
+}
