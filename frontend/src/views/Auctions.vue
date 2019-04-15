35718f3e39924aa64ff40ccc84caee6bc7abaac7
Rickard <rickard.andersson98@gmail.com>
2019-04-15 21:37:13 +0200

    made a list that shows all auctions

--- frontend/src/views/Auctions.vue
+++ frontend/src/views/Auctions.vue
@@ -1,10 +1,24 @@
 <template>
-    
+    <v-flex sm10 offset-sm1 offset-xs mt-2>
+        <h1>All Auctions</h1>
+          <v-layout row wrap>
+        <AuctionCard v-for="auction in auctions" v-bind:key="auction.itemID" :auctionObject="auction"></AuctionCard>
+          </v-layout>
+  </v-flex>
 </template>
 
 <script>
+import AuctionCard from '../components/AuctionCard';
 export default {
     name:'Auctions',
+    computed:{
+    auctions(){
+        return this.$store.getters.getAuctions;
+    }
+  },
+  components:{
+      AuctionCard,
+  }
 }
 </script>
 
