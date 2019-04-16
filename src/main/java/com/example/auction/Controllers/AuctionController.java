766f2fc41791c1815423f795610dd7bf4467cbfb
Louise Joy Frahm <louisejoyfrahm@mail.dk>
2019-04-16 10:30:34 +0200

    Add variables, methods etc. in auction classes

--- src/main/java/com/example/auction/Controllers/AuctionController.java
+++ src/main/java/com/example/auction/Controllers/AuctionController.java
@@ -1,4 +1,15 @@
 package com.example.auction.Controllers;
 
+import com.example.auction.Repositories.AuctionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AuctionController {
+    @Autowired
+    AuctionRepository repo;
+
+    @GetMapping
+    Iterable getPosts(){
+        return repo.findAll();
+    }
 }
--- src/main/java/com/example/auction/Repositories/AuctionRepository.java
+++ src/main/java/com/example/auction/Repositories/AuctionRepository.java
@@ -1,4 +1,13 @@
 package com.example.auction.Repositories;
 
-public class AuctionRepository {
+import com.example.auction.Datamodels.Auction;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuctionRepository extends CrudRepository<Auction, Long> {
+    Auction findByItemID(Long itemID);
+    List<Auction> findAll();
 }
