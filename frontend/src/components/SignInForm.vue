778a7b8d25470541404fc6060dcbf9f237958c1c
Rickard <rickard.andersson98@gmail.com>
2019-04-16 10:36:35 +0200

    popup made check instructions for implementation

---
+++ frontend/src/components/SignInForm.vue
@@ -0,0 +1,16 @@
+<template>
+        <v-flex sm6>
+    <h1>This is the sign in form</h1>
+        </v-flex>
+</template>
+
+<script>
+export default {
+    name: 'signInForm',
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ frontend/src/components/SignUpForm.vue
@@ -0,0 +1,16 @@
+<template>
+     <v-flex sm6>
+    <h1>This is the sign up form</h1>
+        </v-flex>
+</template>
+
+<script>
+export default {
+    name: 'signUpForm',
+}
+</script>
+
+<style>
+
+</style>
+
