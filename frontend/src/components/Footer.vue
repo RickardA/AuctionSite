52842f06f9d96c72c0cf7eb4e550c51e19cb2779
Mathias <mathias.permlid@gmail.com>
2019-04-17 10:15:46 +0200

    Footer
    basic footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-footer dark height="auto">
+        <v-card flat tile class="indigo lighten-1 white--text text-xs-center flex">
+            <v-container grid-list-md text-xs-center>
+                <v-layout row wrap>
+
+                    <v-flex xs6>
+                        <v-card-text>
+
+                            <p>Contact: info@auctionsite.com</p>
+
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-card-text>
+
+                            <v-btn class="mx-3" dark small icon>
+                                <font-awesome-icon size="2x" :icon="['fab', 'twitter']" />
+                            </v-btn>
+                            <v-btn class="mx-3" dark small icon>
+                                <font-awesome-icon size="2x" :icon="['fab', 'facebook']" />
+                            </v-btn>
+                            <v-btn class="mx-3" dark small icon>
+                                <font-awesome-icon size="2x" :icon="['fab', 'pinterest']" />
+                            </v-btn>
+                            <v-btn class="mx-3" dark small icon>
+                                <font-awesome-icon size="2x" :icon="['fab', 'instagram']" />
+                            </v-btn>
+
+                        </v-card-text>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <v-divider></v-divider>
+            <v-card-text class="white--text text-center">
+                &copy;2019 - <strong>Auction Site</strong>
+            </v-card-text>
+
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+        })
+    }
+</script>
+
+<style>
+</style>
