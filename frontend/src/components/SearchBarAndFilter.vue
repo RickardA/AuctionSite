ec81e02dd723903f40e1de110e456b895c4113d3
Johan Schultze <Johan.schultze@gmail.com>
2019-04-17 16:29:26 +0200

    search redirecting to url

--- frontend/src/components/SearchBarAndFilter.vue
+++ frontend/src/components/SearchBarAndFilter.vue
@@ -1,27 +1,29 @@
 <template>
     <div class="content">
         <v-flex md6 offset-md3 class="pr-2 pl-2">
-        <v-layout row wrap>
+            <v-layout row wrap>
 
-    <v-text-field
-            label="Search"
-            prepend-inner-icon="search"
-            solo
-    ></v-text-field>
-<v-btn>GO!</v-btn>
-        </v-layout>
+                <v-text-field
+                        label="Search"
+                        prepend-inner-icon="search"
+                        solo
+                        v-model="userInput"
+                ></v-text-field>
+                <v-btn
+                        :to="/auctions/ + userInput">GO!</v-btn>
+            </v-layout>
         </v-flex>
 
         <v-flex md8 offset-md2>
             <v-layout row class="justify-center">
-    <div class="categories">
-        <v-btn round color="primary">All categories</v-btn>
-        <v-btn round color="success">Arts and crafts</v-btn>
-        <v-btn round color="success">Clothes</v-btn>
-        <v-btn round color="success">Outdoors</v-btn>
-        <v-btn round color="success">Electronics</v-btn>
-        <v-btn round color="success">Other stuff</v-btn>
-    </div>
+                <div class="categories">
+                    <v-btn round color="primary">All categories</v-btn>
+                    <v-btn round color="success">Arts and crafts</v-btn>
+                    <v-btn round color="success">Clothes</v-btn>
+                    <v-btn round color="success">Outdoors</v-btn>
+                    <v-btn round color="success">Electronics</v-btn>
+                    <v-btn round color="success">Other stuff</v-btn>
+                </div>
             </v-layout>
         </v-flex>
 
@@ -32,9 +34,10 @@
 <script>
     export default {
         name: "SearchBarAndFilter",
-        data(){
-            return{
-            toggle_one: 0
+        data() {
+            return {
+                userInput: '',
+                toggle_one: 0
             }
         },
     }
@@ -42,7 +45,7 @@
 
 <style scoped>
 
-    .categories{
+    .categories {
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
