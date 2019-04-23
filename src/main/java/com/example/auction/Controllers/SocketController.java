42f6e47a2e1b489262a743874817e17310b2c8d9
Rickard <rickard.andersson98@gmail.com>
2019-04-23 13:41:47 +0200

    forgot classes

---
+++ src/main/java/com/example/auction/Controllers/SocketController.java
@@ -0,0 +1,37 @@
+package com.example.auction.Controllers;
+
+import com.example.auction.Services.SocketService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+@Controller
+public class SocketController extends TextWebSocketHandler {
+
+    // NOTE: Can not use @Autowired here due to WebSocketConfig instantiating the SocketController
+    private SocketService socketService;
+    public void setSocketService(SocketService socketService) {
+        this.socketService = socketService;
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        System.out.println("recieved message is: "+ message.getPayload());
+
+        socketService.sendToAll("Hello world");
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        socketService.addSession(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        socketService.removeSession(session);
+    }
+}
---
+++ src/main/java/com/example/auction/configurations/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.example.auction.configurations;
+
+import com.example.auction.Controllers.SocketController;
+import com.example.auction.Services.SocketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    SocketService socketService;
+
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        SocketController sc = new SocketController();
+        sc.setSocketService(socketService);
+        registry.addHandler(sc, "/socket")
+                .setAllowedOrigins("*")
+        ;
+    }
+}
