2fe0d36a1e8ac1239dd352151d9e786545d75fc9
Louise Joy Frahm <louisejoyfrahm@mail.dk>
2019-05-02 15:08:24 +0200

    Notifications, wups

---
+++ frontend/src/components/Notification/Notification.vue
@@ -0,0 +1,74 @@
+<template>
+     <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      offset-x
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+        id="scroll-button"
+        fab
+        fixed
+        bottom
+        left
+        @click="openNotification"
+      >
+        <font-awesome-icon size="2x" :icon="['fas', 'bell']" />
+      </v-btn>
+      </template>
+
+      <v-card class="test">
+        <v-list >
+          <v-list-tile>
+              <v-list-tile-title>Your notifications</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list class="list">
+          <NotificationCard />
+        </v-list>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click="menu = false, message = ''">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+</template>
+
+<script>
+import NotificationCard from '../Notification/NotificationCard'
+export default {
+    name: 'notification',
+    components:{
+    NotificationCard
+    },
+     data:() => ({
+      loaded: false,
+        fav: true,
+      menu: false,
+      showList: true,
+      choosenChat: null,
+    }),
+  methods:{
+    openNotification(){
+      this.menu = true;
+    }
+  }
+}
+</script>
+
+<style>
+#scroll-button{
+        margin-bottom: 50px;
+    }
+.list{
+  height: 40vh;
+  overflow: hidden;
+}
+.test{
+  width: 300px !important;
+}
+</style>
---
+++ frontend/src/components/Notification/NotificationCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-card color="indigo lighten-1" class="white--text" @click="openChatWindow">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">{{auctionObject.title}}</div>
+                      <div>Price: {{auctionObject.min_price}}</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    height="125px"
+                    contain
+                  ></v-img>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+            </v-card>
+</template>
+
+<script>
+export default {
+    name: 'notificationCard',
+    props:{
+        auctionObject: Object,
+    },
+    computed:{
+      /*imageURL(){
+        return this.chat.images === undefined ? '' : this.chat.images[0].imgURL;
+      } */
+    },
+    methods:{
+        openChatWindow(){
+          console.log("idiot");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
